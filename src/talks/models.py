c184c82e8f0c9c054af6b6a43e1e80117c6082d6
mahmoudfarweez5 <m.farweez@xware.co>
2021-11-27 12:57:53 +0200

    fix/duration for talk model:

--- src/talks/models.py
+++ src/talks/models.py
@@ -7,7 +7,7 @@ from conferences.models import Conference
 class Talk(TimeStampedModel):
     title = models.CharField("Title", max_length=100)
     description = models.TextField("Description")
-    duration = models.DateField("Duration")
+    duration = models.DurationField("Duration")
     date = models.DateTimeField("Date")
     conference = models.ForeignKey(Conference, on_delete=models.SET_NULL,
                                    null=True, related_name="talk_conference")
